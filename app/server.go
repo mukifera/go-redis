24c12bf3752b24d41be8be0401a0acda40ef0362
Feras Muki <feras.muki@gmail.com>
2024-11-05 13:39:11 -0500

    respond with pong for all messages

--- app/server.go
+++ app/server.go
@@ -9,12 +9,27 @@ import (
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Fprintln(os.Stderr, "Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Fprintf(os.Stderr, "Error accepting connection: %v\n", err)
 		os.Exit(1)
 	}
+
+	buffer := make([]byte, 1024)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to read from connection")
+		os.Exit(1)
+	}
+
+	response := []byte("+PONG\r\n")
+	_, err = conn.Write(response)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to write from connection")
+		os.Exit(1)
+	}
+
 }
