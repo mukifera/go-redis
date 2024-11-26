b840698bd6f91774ab6d8a363fbd0174c8035288
Feras Muki <feras.muki@gmail.com>
2024-11-26 16:41:47 -0500

    export read and generate rdb file functions from the rdb subpackage

--- app/rdb/rdb.go
+++ app/rdb/rdb.go
@@ -63,7 +63,7 @@ var rdbValueTypes = struct {
 	LIST_QUICKLIST:     14,
 }
 
-func readRDBFile(filename string) (*core.Store, error) {
+func ReadFile(filename string) (*core.Store, error) {
 	var store core.Store
 	var current uint64 = 9
 	store.Init()
@@ -281,7 +281,7 @@ func readRDBSet(data []byte) (bytes_read uint64, set map[resp.Object]struct{}) {
 	return
 }
 
-func generateRDBFile(_ *core.Store) []byte {
+func GenerateFile(_ *core.Store) []byte {
 	hex_str := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
 	data, _ := hex.DecodeString(hex_str)
 	return data
