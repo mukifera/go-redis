f23b82ca328170e0b9c8554746fe7a43b3277d00
Feras Muki <feras.muki@gmail.com>
2024-11-26 18:45:02 -0500

    extract SET command handling logic into a separate file

--- app/commands/commands.go
+++ app/commands/commands.go
@@ -100,46 +100,6 @@ func handleEchoCommand(call resp.Array, conn *core.Conn, _ *core.Store) resp.Obj
 	return resp.BulkString(key)
 }
 
-func handleSetCommand(call resp.Array, conn *core.Conn, store *core.Store) resp.Object {
-	if len(call) != 3 && len(call) != 5 {
-		return resp.SimpleError("invalid number of arguments to SET command")
-	}
-	var err error
-	var expiry uint64
-	key, ok := call[1].(resp.BulkString)
-	if !ok {
-		return resp.SimpleError("key must be a string")
-	}
-	value, ok := call[2].(resp.BulkString)
-	if !ok {
-		return resp.SimpleError("value must be a string")
-	}
-	if len(call) == 5 {
-		flag, ok := call[3].(resp.BulkString)
-		if !ok {
-			return resp.SimpleError("expected flag to be a string")
-		}
-		if strings.ToUpper(string(flag)) == "PX" {
-			expiry_str, ok := call[4].(resp.BulkString)
-			if !ok {
-				return resp.SimpleError("expected an expiry value")
-			}
-			expiry, err = strconv.ParseUint(string(expiry_str), 10, 64)
-			if err != nil {
-				return resp.SimpleError(fmt.Sprintf("expected expiry value to be an integer: %v\n", err))
-			}
-		}
-		store.SetWithExpiry(string(key), value, expiry)
-	} else {
-		store.Set(string(key), call[2])
-	}
-
-	if store.Master == nil || conn.Conn != store.Master.Conn {
-		return resp.SimpleString("OK")
-	}
-	return nil
-}
-
 func handleGetCommand(call resp.Array, conn *core.Conn, store *core.Store) resp.Object {
 	if len(call) != 2 {
 		return resp.SimpleError("invalid number of arguments to GET command")
